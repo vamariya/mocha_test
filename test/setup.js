0c5cdbf7da208d9ccda10ffa913c0ec2839820e2
vanitha <vanitha.vanitha1@gmail.com>
2014-04-07 03:38:38 -0700

    setup commit

--- test/setup.js
+++ test/setup.js
@@ -2,7 +2,7 @@
 function setup()
 {
 var fs = require('fs');
-eval(fs.readFileSync("/root/nodeJS/basha/bckup/test/topo.js",encoding="ascii"));
+eval(fs.readFileSync("test/topo.js",encoding="ascii"));
 //var mydata = fs.readFileSync(settings.SOME_FILE);
 console.log("ip addr for the device " + settings.ip);
 };
